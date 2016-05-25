5a76789a6a2b0aa487577288bf526738afe86a74
Mark Douma <mark@markdouma.com>
2016-05-25 14:39:51 -0400

    Updated to add attachment entries for the data referenced by use of the CSS `url()` function.
    Both linked stylesheets and inline styles are adapted.

--- src/CHMQuickLookHTMLDocument.m
+++ src/CHMQuickLookHTMLDocument.m
@@ -12,7 +12,7 @@
 #import <CoreServices/CoreServices.h>
 
 
-#define MD_DEBUG 1
+#define MD_DEBUG 0
 
 #if MD_DEBUG
 #define MDLog(...) NSLog(__VA_ARGS__)
@@ -21,6 +21,14 @@
 #endif
 
 #define MD_DEBUG_DUMP_TO_FILES 0
+
+static NSStringEncoding CHMStringEncodingFromIANAEncodingName(NSString *encodingName) {
+	return CFStringConvertEncodingToNSStringEncoding(CFStringConvertIANACharSetNameToEncoding((CFStringRef)encodingName));
+}
+
+static NSString *CHMIANAEncodingNameFromEncoding(NSStringEncoding encoding) {
+	return (NSString *)CFStringConvertEncodingToIANACharSetName(CFStringConvertNSStringEncodingToEncoding(encoding));
+}
 
 
 @interface NSXMLElement (CHMAdditions)
@@ -166,6 +174,112 @@ static NSString * MDDesktopDebugFolderPath = nil;
 }
 
 
+- (NSString *)stringFromData:(NSData *)data {
+	
+	NSMutableSet *mTriedEncodings = [NSMutableSet set];
+	
+	NSMutableSet *mEncodingNames = [NSMutableSet set];
+	if (document.characterEncoding) [mEncodingNames addObject:document.characterEncoding];
+	if (documentFile.encodingName) [mEncodingNames addObject:documentFile.encodingName];
+	
+	MDLog(@"[%@ %@] mEncodingNames == %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), mEncodingNames);
+	
+	NSArray *encodingNames = mEncodingNames.allObjects;
+	
+	for (NSString *encodingName in encodingNames) {
+		NSStringEncoding nsStringEncoding = CHMStringEncodingFromIANAEncodingName(encodingName);
+		NSString *cssString = [[[NSString alloc] initWithData:data encoding:nsStringEncoding] autorelease];
+		if (cssString) return cssString;
+		[mTriedEncodings addObject:[NSNumber numberWithUnsignedInteger:nsStringEncoding]];
+	}
+	
+	if (![mTriedEncodings containsObject:[NSNumber numberWithUnsignedInteger:NSWindowsCP1252StringEncoding]]) {
+		NSString *cssString = [[[NSString alloc] initWithData:data encoding:NSWindowsCP1252StringEncoding] autorelease];
+		if (cssString) return cssString;
+		[mTriedEncodings addObject:[NSNumber numberWithUnsignedInteger:NSWindowsCP1252StringEncoding]];
+	}
+	if (![mTriedEncodings containsObject:[NSNumber numberWithUnsignedInteger:NSISOLatin1StringEncoding]]) {
+		NSString *cssString = [[[NSString alloc] initWithData:data encoding:NSISOLatin1StringEncoding] autorelease];
+		if (cssString) return cssString;
+		[mTriedEncodings addObject:[NSNumber numberWithUnsignedInteger:NSISOLatin1StringEncoding]];
+	}
+	if (![mTriedEncodings containsObject:[NSNumber numberWithUnsignedInteger:NSUTF8StringEncoding]]) {
+		NSString *cssString = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
+		if (cssString) return cssString;
+		[mTriedEncodings addObject:[NSNumber numberWithUnsignedInteger:NSUTF8StringEncoding]];
+	}
+	
+	return nil;
+}
+
+
+- (void)adaptCSSString:(NSMutableString *)mCSSString inArchiveItem:(CHMArchiveItem *)anItem {
+	
+	MDLog(@"[%@ %@] cssString (BEFORE) == %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), mCSSString);
+	
+	static NSCharacterSet *quotesCharacterSet = nil;
+	if (quotesCharacterSet == nil) quotesCharacterSet = [[NSCharacterSet characterSetWithCharactersInString:@"\"'"] retain];
+	static NSCharacterSet *emptyCharacterSet = nil;
+	if (emptyCharacterSet == nil) emptyCharacterSet = [[NSCharacterSet characterSetWithCharactersInString:@""] retain];
+	
+	NSMutableString *mAdaptedCSSString = [NSMutableString string];
+	
+	NSScanner *scanner = [NSScanner scannerWithString:mCSSString];
+	[scanner setCharactersToBeSkipped:emptyCharacterSet];
+	
+	NSString *result = nil;
+	NSString *URLString = nil;
+	
+	NSUInteger scanLocation = 0;
+	
+	while ([scanner isAtEnd] == NO) {
+		if ([scanner scanUpToString:@"url(" intoString:&result] &&
+			[scanner scanString:@"url(" intoString:NULL] &&
+			[scanner scanUpToString:@")" intoString:&URLString] &&
+			[scanner scanString:@")" intoString:NULL]) {
+			
+			if (result.length) [mAdaptedCSSString appendString:result];
+			scanLocation = scanner.scanLocation;
+			
+			NSString *quotelessURLString = [URLString stringByTrimmingCharactersInSet:quotesCharacterSet];
+			
+			MDLog(@"[%@ %@] quotelessURLString == \"%@\"", NSStringFromClass([self class]), NSStringFromSelector(_cmd), quotelessURLString);
+			
+			CHMArchiveItem *urlArchiveItem = [documentFile archiveItemAtPath:URLString relativeToArchiveItem:anItem];
+			NSData *itemData = urlArchiveItem.data;
+			if (urlArchiveItem == nil || itemData == nil) {
+				[mAdaptedCSSString appendString:[NSString stringWithFormat:@"url(%@)", URLString]];
+				continue;
+			}
+			
+			[mAdaptedCSSString appendString:[NSString stringWithFormat:@"url(\"cid:%@\")", quotelessURLString]];
+			
+			NSString *MIMEType = [CHMArchiveItem MIMETypeForPathExtension:urlArchiveItem.pathExtension];
+			
+			NSMutableDictionary *attachments = [self attachmentsDictionary];
+			NSMutableDictionary *attachmentEntry = [NSMutableDictionary dictionaryWithObjectsAndKeys:itemData,(id)kQLPreviewPropertyAttachmentDataKey, nil];
+			if (MIMEType) [attachmentEntry setObject:MIMEType forKey:(id)kQLPreviewPropertyMIMETypeKey];
+			[attachments setObject:attachmentEntry forKey:URLString];
+			
+		} else {
+			[mAdaptedCSSString appendString:[mCSSString substringFromIndex:scanLocation]];
+		}
+	}
+	
+	MDLog(@"[%@ %@] cssString (AFTER) == %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), mAdaptedCSSString);
+	
+	[mCSSString setString:mAdaptedCSSString];
+}
+
+
+- (void)adaptStyleElement:(NSXMLElement *)styleElement {
+	NSMutableString *mCSSString = [[styleElement.stringValue mutableCopy] autorelease];
+	[self adaptCSSString:mCSSString inArchiveItem:linkItem.archiveItem];
+	styleElement.stringValue = mCSSString;
+}
+
+
+
 - (void)adaptLinkElement:(NSXMLElement *)linkElement {
 	NSXMLNode *typeAttr = [linkElement chm__attributeForCaseInsensitiveName:@"type"];
 	NSXMLNode *hrefAttr = [linkElement chm__attributeForCaseInsensitiveName:@"href"];
@@ -181,13 +295,27 @@ static NSString * MDDesktopDebugFolderPath = nil;
 		
 		return;
 	}
+	
+	NSString *cssString = [self stringFromData:cssData];
+	
+	NSData *adaptedCSSData = cssData;
+	NSString *adaptedCSSStringEncodingName = documentFile.encodingName;
+	
+	if (cssString) {
+		NSMutableString *mCSSString = [[cssString mutableCopy] autorelease];
+		[self adaptCSSString:mCSSString inArchiveItem:cssItem];
+		
+		adaptedCSSData = [mCSSString dataUsingEncoding:NSUTF8StringEncoding];
+		adaptedCSSStringEncodingName = CHMIANAEncodingNameFromEncoding(NSUTF8StringEncoding);
+	}
+	
 	NSString *contentIDFilePath = [@"cid:" stringByAppendingString:cssFilePath];
 	hrefAttr.stringValue = contentIDFilePath;
 	
 	NSMutableDictionary *attachments = [self attachmentsDictionary];
-	NSMutableDictionary *attachmentEntry = [NSMutableDictionary dictionaryWithObjectsAndKeys:cssData,(id)kQLPreviewPropertyAttachmentDataKey,
+	NSMutableDictionary *attachmentEntry = [NSMutableDictionary dictionaryWithObjectsAndKeys:adaptedCSSData,(id)kQLPreviewPropertyAttachmentDataKey,
 											@"text/css",(id)kQLPreviewPropertyMIMETypeKey, nil];
-	if (documentFile.encodingName) [attachmentEntry setObject:documentFile.encodingName forKey:(id)kQLPreviewPropertyTextEncodingNameKey];
+	if (adaptedCSSStringEncodingName) [attachmentEntry setObject:adaptedCSSStringEncodingName forKey:(id)kQLPreviewPropertyTextEncodingNameKey];
 	[attachments setObject:attachmentEntry forKey:cssFilePath];
 	
 }
@@ -216,8 +344,8 @@ static NSString * MDDesktopDebugFolderPath = nil;
 }
 
 
+
 - (void)adaptHTML {
-	
 #if MD_DEBUG_DUMP_TO_FILES
 	[self writeDebugDataToDebugPathWithName:@"chm__Before.html"];
 #endif
@@ -241,8 +369,18 @@ static NSString * MDDesktopDebugFolderPath = nil;
 				
 			} else if ([elementName isEqualToString:@"img"]) {
 				[self adaptImageElement:element];
+				
+			} else if ([elementName isEqualToString:@"style"]) {
+				[self adaptStyleElement:element];
+				
 			}
 			
+			NSXMLNode *styleAttr = [element chm__attributeForCaseInsensitiveName:@"style"];
+			if (styleAttr) {
+				NSMutableString *mCSSString = [[styleAttr.stringValue mutableCopy] autorelease];
+				[self adaptCSSString:mCSSString inArchiveItem:linkItem.archiveItem];
+				styleAttr.stringValue = mCSSString;
+			}
 		}
 	}
 	
